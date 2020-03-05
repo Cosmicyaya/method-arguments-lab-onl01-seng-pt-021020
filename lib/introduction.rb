d5b219583a91fb6b924acaa01d9f277ee2462a18
Ryan Hollins <ballbaroi@gmail.com>
2020-03-05 16:54:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,6 @@
-# lib/introduction.rb
-
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is Dan and I am learning to program in Ember.js."
 end
